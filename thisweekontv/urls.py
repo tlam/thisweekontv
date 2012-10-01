341146182cd052cdb284f921f853edb4edb16055
Thierry Lam <lamthierry@gmail.com>
2012-09-30 23:31:22 -0400

    Using gunicorn for production server

--- thisweekontv/urls.py
+++ thisweekontv/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.autodiscover()
@@ -10,3 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
