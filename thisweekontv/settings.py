ec14134994815e7df063d23e74a3b5751bb25aee
Thierry Lam <lamthierry@gmail.com>
2012-09-30 23:20:20 -0400

    Specifying STATIC_ROOT

--- thisweekontv/settings.py
+++ thisweekontv/settings.py
@@ -3,7 +3,6 @@ import os
 import dj_database_url
 
 ROOT_PATH = os.path.dirname(__file__)
-print ROOT_PATH
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -68,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(ROOT_PATH, 'static_files')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
