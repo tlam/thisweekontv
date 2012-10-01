9265860c50fc51783fda520bba189cd4b1458492
Thierry Lam <lamthierry@gmail.com>
2012-09-30 23:38:55 -0400

    Specifying static path to read from heroku

--- thisweekontv/urls.py
+++ thisweekontv/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.autodiscover()
@@ -12,4 +12,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
+
+
