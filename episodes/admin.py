9942c971d3d082dfbb73bd2f0598ec37714ccf9a
Thierry Lam <lamthierry@gmail.com>
2012-10-09 20:18:55 -0400

    Adding action to mark episode as seen from admin

--- episodes/admin.py
+++ episodes/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from episodes.models import Episode
 
+
+def mark_as_seen(modeladmin, request, queryset):
+    queryset.update(has_seen=True)
+mark_as_seen.short_description = 'Mark selected episodes as seen'
+
 class EpisodeAdmin(admin.ModelAdmin):
     list_display = ('show', 'number', 'air_date', 'has_seen',)
     list_filter = ('show',)
+    actions = [mark_as_seen]
 
 admin.site.register(Episode, EpisodeAdmin)
